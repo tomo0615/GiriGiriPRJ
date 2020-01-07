9b413188b7c70e6ad9f688435726ab680d4e03ab
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-01-08 00:13:53 +0900

    TitleUI　SceneManagerを実装

---
+++ Assets/Scripts/Managers/LoadSceneManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.SceneManagement;
+
+public enum SceneType 
+{
+    Title,
+    Game
+}
+
+public class LoadSceneManager : SingletonMonoBehaviour<LoadSceneManager>
+{
+    private string sceneName = "";
+
+    void Start()
+    {
+        DontDestroyOnLoad(this); //共通で使うため
+    }
+
+    private void LoadGameScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    #region　UIボタン用関数
+    public void OnClickSceneButton(string name)
+    {
+        sceneName = name;
+
+        LoadGameScene();
+    }
+    #endregion
+
+    #region 他クラス呼び出し用
+    public void OnLoadGameScene(SceneType scene)
+    {
+        sceneName = scene.ToString();
+
+        LoadGameScene();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Title/AnyKeyToStart.cs
@@ -0,0 +1,14 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class AnyKeyToStart : MonoBehaviour
+{
+    private void Start()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => Input.anyKey)
+            .Subscribe(_ => 
+            LoadSceneManager.Instance.OnLoadGameScene(SceneType.Game));
+    }
+}
