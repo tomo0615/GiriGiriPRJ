23d40e1040b9162b1ed7579b7e3fd84b18bc637f
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-01-20 02:30:10 +0900

    バグの修正

--- Assets/Scripts/Players/PlayerController.cs
+++ Assets/Scripts/Players/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour,IPlayerCollider
     private PlayerAnimator _playerAnimator;
 
     [SerializeField]
-    private GiriGiriCollider _collider = null;
+    private GiriGiriCollider _giriGiriCollider = null;
+
+    [SerializeField]
+    private YoketaCollider _yoketaCollider = null;
 
     [SerializeField]
     private CameraController _cameraController = null;
@@ -39,22 +42,23 @@ public class PlayerController : MonoBehaviour,IPlayerCollider
 
                 _playerAnimator.DOMoveAnimation();
 
-                StartCoroutine(_collider.SwitchCollider());//ギリギリ判定を出す
+                StartCoroutine(_giriGiriCollider.SwitchCollider());//ギリギリ判定を出す
             });
     }
 
     public void Collided()
     {
-        _cameraController.ShakeCamera(10f);
+        _cameraController.ShakeCamera(5f);
 
+        //死亡演出
         GameEffectManager.Instance.OnGenelateEffect(transform.position, EffectType.DeadEffect);
+        SoundManager.Instance.PlaySoundOneShot(SoundType.dead);
 
+        //Scoreが入らないように
+        _yoketaCollider.gameObject.SetActive(false);
         Destroy(gameObject);
 
         GameStateManager.Instance.SetGameState(GameState.End);
-
-        SoundManager.Instance.PlaySoundOneShot(SoundType.dead);
-
-        Debug.Log("GameOver");
+        //Debug.Log("GameOver");
     }
 }
