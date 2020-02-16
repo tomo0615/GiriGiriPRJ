361b32e9c57c0dca0cf854d3909f48c385690951
Chomo <the.gamut.of.human.feelings@gmail.com>
2020-02-16 14:34:09 +0900

    TitleのWindow切り替えを実装完了

--- Assets/Scripts/GUI/TitleUI/TitleUIButton.cs
+++ Assets/Scripts/GUI/TitleUI/TitleUIButton.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using DG.Tweening;
 
 public class TitleUIButton : BaseButton
@@ -9,15 +6,21 @@ public class TitleUIButton : BaseButton
     [SerializeField]
     private RectTransform displayWindow = null;
 
+    [SerializeField]
+    private bool displayableWindow = true;
+
     protected override void OnPushed()
     {
         base.OnPushed();
 
         //Windowの表示
+        SetActiveWindow();
+        //Windowの表示アニメーション
+
     }
 
     private void SetActiveWindow()
     {
-
+        displayWindow.gameObject.SetActive(displayableWindow);
     }
 }
